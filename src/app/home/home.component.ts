b49c3cc10ad687e0473aff71bd99a636f260ee59
doan.kieu <doan.kieu@manifera.com>
2022-08-18 17:23:10 +0700

    ngClass, ngFor, ngIf

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public name = 'Doan Kieu';
   public age = 25;
+  public traiCay = ["Đào", "Nho", "Bưởi", "Cam", "Quýt"];
+  public traiCay2 = [
+    {ten: "Táo", gia:12, haGia: true}, 
+    {ten: "Nho", gia:3, haGia: false}, 
+    {ten: "Bưởi", gia: -10, haGia: true}
+  ];
 
   constructor() { }
 
