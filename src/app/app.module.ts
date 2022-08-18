4b6f61c0fa086981fc0e7f5f7bd907a6c175a348
doan.kieu <doan.kieu@manifera.com>
2022-08-18 21:42:03 +0700

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
